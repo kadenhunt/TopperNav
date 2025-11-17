9fb761404f99fc51964fe30e822534b2f3bf0116
Ajdowning06 <ajdowning06@gmail.com>
2025-11-17 17:27:12 -0600

    toppernavdatabase java file added code

---
+++ TopperNavApp/app/src/main/java/edu/wku/toppernav/data/local/db/TopperNavDatabase.java
@@ -0,0 +1,38 @@
+package edu.wku.toppernav.data.local.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import edu.wku.toppernav.data.local.dao.RoomDao;
+import edu.wku.toppernav.data.local.entity.RoomEntity;
+
+@Database(
+    entities = {RoomEntity.class},
+    version = 1,
+    exportSchema = false
+)
+
+public abstract class TopperNavDatabase extends RoomDatabase {
+
+    public abstract RoomDao roomDao();
+
+    private static volatile TopperNavDatabase INSTANCE;
+
+    public static TopperNavDatabase getInstance(Context content) {
+        if (INSTANCE == null) {
+            synchronized (TopperNavDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.getApplicationContext(),
+                        TopperNavDatabase.class,
+                        "toppernav.db"
+                    ).build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
